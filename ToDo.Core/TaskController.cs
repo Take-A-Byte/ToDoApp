a943b8a02c13de13704e750c796991061948639d
Shantanu Methikar <smethikar18@gmail.com>
2022-07-16 09:13:59 +0200

    Core - create core project

---
+++ ToDo.Core/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using ToDo.API;
+
+namespace ToDo.Core
+{
+    public class TaskController : ITaskController
+    {
+        public Task AddTask(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IToDoTask> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDo.Core/ToDoTask.cs
@@ -0,0 +1,10 @@
+﻿using ToDo.API;
+
+namespace ToDo.Core
+{
+    public class ToDoTask : IToDoTask
+    {
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool HasCompleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
