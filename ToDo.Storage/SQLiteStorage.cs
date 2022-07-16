81527624c18a54cc765e9a2d872f2abd1db96655
Shantanu Methikar <smethikar18@gmail.com>
2022-07-16 09:14:35 +0200

    Storage - Create storage project

---
+++ ToDo.Storage/SQLiteStorage.cs
@@ -0,0 +1,32 @@
+﻿using ToDo.API;
+
+namespace ToDo.Storage
+{
+    public class SQLiteStorage : ITaskStorage
+    {
+        public Task<bool> AddNewTask(long taskId, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetTask(long taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTaskCompleteness(long taskId, bool completeness)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTaskDescription(long taskId, string description)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
