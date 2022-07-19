8369426e08eda79d88ecaac7a8c6cbfa105b4bdd
Shantanu Methikar <smethikar18@gmail.com>
2022-07-19 11:21:22 +0200

    API - Add documentation for APIs

--- ToDo.API/IToDoTask.cs
+++ ToDo.API/IToDoTask.cs
@@ -2,8 +2,19 @@
 {
     public interface IToDoTask
     {
+        /// <summary>
+        /// Unique number representing the task
+        /// </summary>
         long Id { get; }
+
+        /// <summary>
+        /// Description of the task
+        /// </summary>
         string Description { get; set; }
+
+        /// <summary>
+        /// Boolean representing if the task has been completed or not
+        /// </summary>
         bool HasCompleted { get; set; }
     }
 }
